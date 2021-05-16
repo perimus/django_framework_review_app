a45492374ab186890e3d777df5416dd74599755a
sebastien <roegiers.sebastien@hotmail.com>
2021-05-16 11:45:06 +0900

    db: Use local postgresql db

--- bookreviewr/bookreviewr/settings.py
+++ bookreviewr/bookreviewr/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = "bookreviewr.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        'OPTIONS' : {
+            'options': '-c search_path=bookreviewr,public'
+        },
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "docker",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
