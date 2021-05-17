3f9cf49592358832023f7d4c6d9b4ccc9f8ee004
sebastien <roegiers.sebastien@hotmail.com>
2021-05-17 11:49:46 +0900

    add: url mapping for book detail

--- bookreviewr/reviews/urls.py
+++ bookreviewr/reviews/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from reviews.views import book_list
+from reviews.views import books_list, book_detail
+
 
 urlpatterns = [
-    path("books/", book_list, name="book_list"),
+    path("books/", books_list, name="books_list"),
+    path("book/<int:book_id>/", book_detail, name="book_detail"),
 ]
