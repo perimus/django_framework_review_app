71a84214e792b82ca0d3c3063ac03b74ece80265
sebastien <roegiers.sebastien@hotmail.com>
2021-05-18 09:37:26 +0900

    add: Custom AdminSite model

--- bookreviewr/reviews/admin.py
+++ bookreviewr/reviews/admin.py
@@ -1,8 +1,15 @@
-from django.contrib import admin
+from django.contrib.admin import AdminSite
 from reviews.models import DBBook, DBBookContributor, DBContributor, DBPublisher, DBReview
 
-admin.site.register(DBPublisher)
-admin.site.register(DBContributor)
-admin.site.register(DBBook)
-admin.site.register(DBBookContributor)
-admin.site.register(DBReview)
+class BookreviewrAdminSite(AdminSite):
+    title_header: str = "Bookreviewr Admin"
+    side_header: str = "Bookreviewr site Admin"
+
+admin_site: BookreviewrAdminSite = BookreviewrAdminSite(name="bookreviewr")
+
+# Registration of Bookreviewr Models
+admin_site.register(DBPublisher)
+admin_site.register(DBContributor)
+admin_site.register(DBBook)
+admin_site.register(DBBookContributor)
+admin_site.register(DBReview)
