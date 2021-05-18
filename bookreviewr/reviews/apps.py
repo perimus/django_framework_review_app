54f188a43da84b4579bc665bb9ab8f6e3cc393d5
sebastien <roegiers.sebastien@hotmail.com>
2021-05-18 10:30:13 +0900

    pre-commit: Pre-commit changes

--- bookreviewr/reviews/apps.py
+++ bookreviewr/reviews/apps.py
@@ -6,5 +6,6 @@ class ReviewsConfig(AppConfig):
     default_auto_field: str = "django.db.models.BigAutoField"
     name: str = "reviews"
 
+
 class ReviewsAdminConfig(SimpleAdminConfig):
     default_site: str = "bookreviewr.admin.BookreviewrAdminSite"
