129d90fc3effedaa4e5fb3c7149b60fe271fda9b
sebastien <roegiers.sebastien@hotmail.com>
2021-05-21 11:56:10 +0900

    pre-commit: Pre-commit changes

--- bookreviewr/reviews/urls.py
+++ bookreviewr/reviews/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from reviews.views import book_detail, books_list
 from django.views.generic.base import TemplateView
+from reviews.views import book_detail, books_list
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name='base.html'), name='home'),
+    path("", TemplateView.as_view(template_name="base.html"), name="home"),
     path("books/", books_list, name="books_list"),
     path("book/<int:book_id>/", book_detail, name="book_detail"),
 ]
