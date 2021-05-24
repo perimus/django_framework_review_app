2eb3477b408665ebc5e10caf8af40511539821ec
sebastien <roegiers.sebastien@hotmail.com>
2021-05-24 09:05:31 +0900

    add: Test form without CSRF Token

--- form_project/form_example/views.py
+++ form_project/form_example/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+
+from django.shortcuts import render
+from django.http import request
+
+def form_example(request: request) -> render:
+    return render(request, "form-example.html") 
