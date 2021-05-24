d1cbcb415d33686f2f0b38bea984df4f11d5ac5f
sebastien <roegiers.sebastien@hotmail.com>
2021-05-25 08:55:07 +0900

    add: Server side validation for form fields

--- form_project/form_example/views.py
+++ form_project/form_example/views.py
@@ -7,7 +7,11 @@ from .forms import ExampleForm
 
 
 def form_example(request: request) -> render:
-    form = ExampleForm()
-    for name in request.POST:
-        print(f"{name}: {request.POST.getlist(name)}")
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            for name, value in form.cleaned_data.items():
+                print(f"{name}: ({type(value)}) {value}")
+    else:
+        form = ExampleForm()
     return render(request, "form-example.html", {"method": request.method, "form": form})
