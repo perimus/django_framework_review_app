88f0b9b632c8aadd9af2cd1b7f6490d29b1e0b22
sebastien <roegiers.sebastien@hotmail.com>
2021-05-25 09:09:14 +0900

    add: SearchForm logic

---
+++ bookreviewr/reviews/forms/books.py
@@ -0,0 +1,15 @@
+from django import forms
+
+
+class SearchForm(forms.Form):
+    """
+    """
+
+    SEARCH_CHOICES= (
+        ("title", "Title"),
+        ("contributor", "Contributor"),
+    )
+
+    search = forms.CharField(min_length=3, required=False)
+    search_in = forms.ChoiceField(choices=[CHOICES], required=False)
+    
