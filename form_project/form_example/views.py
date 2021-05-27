a8e45ed3e4a7eaee572bb693836e55d0dc1bd433
sebastien <roegiers.sebastien@hotmail.com>
2021-05-27 11:00:48 +0900

    add: Form field placeholders & initial values.

--- form_project/form_example/views.py
+++ form_project/form_example/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 
 from .forms import OrderForm
 
+initial = {"email": "user@example.com"}
 
 def form_example(request: request) -> render:
     if request.method == "POST":
@@ -13,5 +14,5 @@ def form_example(request: request) -> render:
             for name, value in form.cleaned_data.items():
                 print(f"{name}: ({type(value)}) {value}")
     else:
-        form = OrderForm()
+        form = OrderForm(initial=initial)
     return render(request, "form-example.html", {"method": request.method, "form": form})
